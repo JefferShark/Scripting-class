4bc18fc7ce1e89a665e83e35a85b9d258e2e6f9e
unknown <10735140@CS514-09.ad.uvu.edu>
2016-02-22 08:15:15 -0700

    scope and axis modifiers

---
+++ DerpStick/Assets/script folder/AnotherClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass : MonoBehaviour 
+{
+	public int apples;
+	public int bananas;
+	
+	
+	private int pie;
+	private int cookies;
+	
+	
+	public void FruitMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Ammo total: " + answer);
+	}
+	
+	
+	private void BakerBot (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Total baking capabilities: " + answer);
+	}
+}
