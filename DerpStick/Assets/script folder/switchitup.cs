7024ec38a28bb6a9b7e84fa13a703b616029b8c1
unknown <10735140@CS514-09.ad.uvu.edu>
2016-03-30 09:03:42 -0600

    switch statements

---
+++ DerpStick/Assets/script folder/switchitup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchitup : MonoBehaviour 
+{
+	
+	public enum directions {
+		Noot,
+		Sandwich,
+		Ease,
+		Wargs
+	}
+	
+	
+	void ChangeDirection(directions _d) {
+		currentDirection = _d;
+		print (currentDirection);
+	}
+	
+	public directions currentDirection;
+	
+	void OnMouseDown () {
+		
+		switch (currentDirection) {
+		case directions.Noot:
+			ChangeDirection (directions.Sandwich);
+			break;
+			
+		case directions.Sandwich:
+			ChangeDirection (directions.Ease);
+			break;
+			
+		case directions.Ease:
+			ChangeDirection (directions.Wargs);
+			break;
+			
+		case directions.Wargs:
+			ChangeDirection (directions.Noot);
+			break;
+			
+		}
+	}
+}
